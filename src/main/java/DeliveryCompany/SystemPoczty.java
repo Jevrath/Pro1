1b47cd6e2801377327cf732c82bb03a80f6b72c6
Kordian <kordiankurdziel@gmail.com>
2016-05-11 12:05:27 +0200

    poprawiona fasada

--- src/main/java/DeliveryCompany/SystemPoczty.java
+++ src/main/java/DeliveryCompany/SystemPoczty.java
@@ -3,21 +3,19 @@ package DeliveryCompany;
 import Customer.Odbiorca;
 
 public class SystemPoczty {
-	
+	Sortownia sort1 = Sortownia.getSortownia();
+	Oddzial oddzial1 = Sortownia.oddzialList.get(0);
 	public void wyslij(String typ, Odbiorca odbiorca)
 	{
-		Sortownia sort1 = Sortownia.getSortownia();
-		Oddzial oddzial1 = Sortownia.oddzialList.get(0);
 				//oddzial2 = Sortownia.oddzialList.get(1);
-		
+		System.out.println("TEST!!!!!!!!!!!!!!");
 		oddzial1.addDelivery(typ, odbiorca);
 		
 	}
 	
 	public void zakoncz()
 	{
-		Sortownia sort1 = Sortownia.getSortownia();
-		Oddzial oddzial1 = Sortownia.oddzialList.get(0);
+		
 		oddzial1.sendToSort();
 		sort1.sendToOddzialy();
 		
